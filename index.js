639de47fc6a5b2b753ddf67334c898a8cde37471
dony01001 <129567018+dony01001@users.noreply.github.com>
2023-12-21 17:08:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-var randomNumber1 = Math.floor(Math.random() * 20) + 1;
-var selector1 = document.querySelectorAll("img")[0];
-var randomDiceImage = "img" + randomNumber1 + "Purple.png";
-var randomeImageSource = "images/" + randomDiceImage;
+var selector1 = document.querySelector(".img1");
 
-selector1.setAttribute("src", randomeImageSource);
+document.querySelector(".img1").addEventListener("click", function () {
+    
+    animationDice();
+    diceNumber();
+    console.log("click");
+    
+})
 
-document.querySelector("h1").innerHTML = "You got a " + randomNumber1 + " !";
+
+function diceNumber() {
+    var randomNumber1 = Math.floor(Math.random() * 20) + 1;
+    var randomDiceImage = "img" + randomNumber1 + "Purple.png";
+    var randomeImageSource = "images/" + randomDiceImage;
+
+    selector1.setAttribute("src", randomeImageSource);
+    document.querySelector("h1").innerHTML = "You got a " + randomNumber1 + " !";
+
+}
+
+function animationDice() {
+    selector1.classList.add("pressed");
+    setTimeout(
+        function(){
+            selector1.classList.remove("pressed");
+        },
+        100
+    )
+}
+
+
